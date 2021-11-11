5df028da54157b0eb11ac18d9383b71e6f829a9f
Bryn <bryn2009@live.co.uk>
2021-11-11 10:57:02 +0000

    further refactored

--- directory.rb
+++ directory.rb
@@ -1,41 +1,7 @@
 @students = []
-def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-end
-
-def show_students
-    print_header
-    print_students_list(@students)
-    print_footer(@students)
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-  
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  @students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -50,8 +16,38 @@ def input_students
     # get another name from the user
     name = gets.chomp
   end
-    # return the array of students
-  @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
 end
 
 def print_header
@@ -59,13 +55,13 @@ def print_header
   puts "-------------"
 end
 
-def print_students_list(students)
+def print_students_list
   @students.each.with_index(1) do |student, index|
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
+def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
